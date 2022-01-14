17bfab70b0263fd5bd0408bd6059ed0ece31ce1e
Marcelo Silva <93952451+MarceloSilva2021@users.noreply.github.com>
2022-01-14 19:27:48 -0300

    FirstCommit
    Meu primeiro Commit.

---
+++ main.js
@@ -0,0 +1,39 @@
+// variaveis do Javascript: let(mutável) ou const (imutável).
+        // O objeto é um grupo de propriedades e valores agrupados.
+
+        const linksSocialMedia = {
+          github: "MarceloSilva2021",
+          whatsapp: "5511964535048",
+          gmail: "marcelomarcolinocvc@gmail.com",
+          facebook: "marcelo.marcolinodasilva",
+          instagram: "marcelo.marcolinodasilva"
+        }
+
+        function changeSocialMediaLinks(){
+          
+          for(let li of socialLinks.children){
+            const social = li.getAttribute("class")
+
+            li.children[0].href = `http://${social}.com/${linksSocialMedia[social]}`            
+          }
+        }
+
+        changeSocialMediaLinks()
+
+        function getGitHubProfileInfos(){
+          const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+          fetch(url)
+          .then(response => response.json())
+          .then (data => {
+            username.textContent = data.name
+            bio.textContent = data.bio
+            usernameGithub.href = data.html_url
+            profilePhoto.src = data.avatar_url
+            userLogin.textContent = data.login
+          })
+
+          
+        }
+
+        getGitHubProfileInfos()
